f4ff921172ae8176da4148d2dd0c7a7e1efd5c45
sebas1913 <sebasosornicastrillon@gmail.com>
2025-01-08 12:20:45 -0500

    Create hello world

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google';
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Hello world!",
+    description: "Sebastián Osorno Castrillón.",
+};
+const poppins = Poppins({ subsets: ["latin"], weight: "500" })
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={poppins.className}>
+                {children}
+            </body>
+        </html>
+    );
+}
